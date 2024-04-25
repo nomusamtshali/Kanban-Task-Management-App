0aa6cc067ac0d44513eb93bbc3eba89323869980
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-04-25 03:16:03 +0200

    changed the one equal operator ( = ) to double equals operator ( == ) to check for comparison

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function displayBoards(boards) {
 // TASK: Fix Bugs
 function filterAndDisplayTasksByBoard(boardName) {
   const tasks = getTasks(); // Fetch tasks from a simulated local storage function
-  const filteredTasks = tasks.filter(task => task.board = boardName);
+  const filteredTasks = tasks.filter(task => task.board == boardName); // instead of assigning a value, I used double equals operator (==) for comparison and to check if the values on both sides are equal. 
 
   // Ensure the column titles are set outside of this function or correctly initialized before this function runs
 
