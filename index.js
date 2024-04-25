5c51eca856e723586f8059337529b90cd0c88419
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-04-25 03:18:31 +0200

    changed the one equal operator ( =) to double equals operator (==) for comparison and to check if the values on both sides are equal.

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function filterAndDisplayTasksByBoard(boardName) {
     const tasksContainer = document.createElement("div");
     column.appendChild(tasksContainer);
 
-    filteredTasks.filter(task => task.status = status).forEach(task => { 
+    filteredTasks.filter(task => task.status == status).forEach(task => { // instead of assigning a value, I used double equals operator (==) for comparison and to check if the values on both sides are equal. 
       const taskElement = document.createElement("div");
       taskElement.classList.add("task-div");
       taskElement.textContent = task.title;
